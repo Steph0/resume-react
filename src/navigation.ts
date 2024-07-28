c894ed6ab879bc7ba6a4b7a60ad0872f8f46d0be
Steph0 <Steph0@users.noreply.github.com>
2024-07-28 16:26:29 +0200

    chore(#2): replacing deprecated Pathnames

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,8 +1,10 @@
 'use client'
 import {
   createLocalizedPathnamesNavigation,
-  Pathnames
 } from 'next-intl/navigation'
+import {
+  Pathnames
+} from 'next-intl/routing'
 import { locales } from './i18n'
 
 export const localePrefix = 'always'
